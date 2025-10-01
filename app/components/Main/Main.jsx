6ee7bc99e357c670b180c325f4031ebb8832f1f3
nemeskalito <borisenko.igor2021@yandex.ru>
2025-10-01 15:39:40 +0300

    added input

---
+++ app/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import './Main.css'
+
+const Main = () => {
+	return (
+		<div className='main'>
+			<div className='input__block'>
+				<input className='input' type='text' />
+				<button className='input__btn'></button>
+			</div>
+		</div>
+	)
+}
+export default Main
