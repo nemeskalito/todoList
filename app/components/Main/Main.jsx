4606604d931a731e1d4d0f85ce11d1dc4ca19828
nemeskalito <borisenko.igor2021@yandex.ru>
2025-10-02 08:52:53 +0300

    feat: added taskList

--- app/components/Main/Main.jsx
+++ app/components/Main/Main.jsx
@@ -1,13 +1,41 @@
-import './Main.css'
+"use client";
+import { useState } from "react";
+import "./Main.css";
 
 const Main = () => {
-	return (
-		<div className='main'>
-			<div className='input__block'>
-				<input className='input' type='text' />
-				<button className='input__btn'></button>
-			</div>
-		</div>
-	)
-}
-export default Main
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleClick = () => {
+    if (inputValue.trim() !== "") {
+      setTasks([...tasks, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="addTask">
+          <input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="addTask__input"
+            type="text"
+          />
+          <button onClick={handleClick} className="addTask__btn">
+            Добавить
+          </button>
+        </div>
+        <div className="taskList">
+          {tasks.map((task, index) => (
+            <div key={task} className="taskList__item">
+              {index + 1}. {task}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Main;
