7214375d35525fd64a3f6d568138fbad03c736d7
nemeskalito <borisenko.igor2021@yandex.ru>
2025-10-14 12:58:44 +0300

    feat: added clearTask

--- app/components/ClearTaskList/ClearTaskList.jsx
+++ app/components/ClearTaskList/ClearTaskList.jsx
@@ -1,13 +1,22 @@
-"use client";
-import "./ClearTaskList.css";
+'use client'
+import deleteTodo from '@/app/api/deleteTodo'
+import './ClearTaskList.css'
 
-const ClearTaskList = ({ filteredTasks, tasks, setTasks }) => {
-  return (
-    <div className="footer">
-      Кол-во: {filteredTasks.length}
-      <button className="btn">Очистить завeршенные</button>
-    </div>
-  );
-};
+const ClearTaskList = ({ filteredTasks, setTasks }) => {
+	const clearList = async () => {
+		setTasks(tasks => {
+			tasks.filter(task => task.isCompleted).forEach(task => deleteTodo(task))
+			return tasks.filter(task => !task.isCompleted)
+		})
+	}
+	return (
+		<div className='footer'>
+			Кол-во: {filteredTasks.length}
+			<button className='btn' onClick={clearList}>
+				Очистить завeршённые
+			</button>
+		</div>
+	)
+}
 
-export default ClearTaskList;
+export default ClearTaskList
